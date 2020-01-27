5049d3646f25fd8bb6040888ffbe4198fa91ce22
kwicz and Matt Taylor <matt@>
2020-01-27 15:54:08 -0800

    add html form and submit event

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Business on top
-function ToDoItemList() {
+function ItemList() {
   this.currentId = 0;
   this.toDoItems = [];
 };
 
-ToDoItemList.prototype.addItem = function(item) {
+ItemList.prototype.addItem = function(item) {
   item.id = this.currentId;
   this.toDoItems.push(item);
   this.currentId++;
@@ -17,7 +17,32 @@ function toDoItem(task, done){
 
 // User on the bottom
 $(document).ready(function(){
-  var itemList = new itemList();
+  var itemList = new ItemList();
 
-  
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var newItem = new toDoItem($("#input").val(), false);
+    itemList.addItem(newItem);
+    $("#not-done").append("<input id='check' type='checkbox'> " + newItem.task);
+    $("form").val("");
+  });
+
+  $("#not-done").on("click", "input[type='checkbox']", function() {
+    if($(this).is(":checked")) {
+      var done = true;
+      console.log(typeof done);
+      console.log($(this).parent());
+      $(this).parent().detach();
+      $("#done").append($(this).parent());
+    } else {
+      var done = false;
+      console.log(typeof done);
+
+      console.log("this: " + $(this).val());
+      $("#not-done").append($(this).parent().val());
+      $(this).parent().appendTo($("#not-done"));
+      $(this).parent().detach();
+    }
+     
+  });
 });
